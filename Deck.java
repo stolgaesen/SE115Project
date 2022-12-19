46e9b330d24d1fa8aef13484c36d29ebe26eb2bc
Samet Tolga Esen <samettolga@gmail.com>
2022-12-19 11:54:00 +0300

    All things have done unless cutting the deck
    i'm working on cutting the deck right now

--- Deck.java
+++ Deck.java
@@ -1,14 +1,36 @@
+import java.util.Random;
 public class Deck{
-	int decknumber = 52;
-	String[] Type = { "♠" , "♣" , "♥" , "♦"};
-	String[] Number={"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
-	int[] deck = new int[decknumber];
+	public Card[] deck = new Card[52];
+	private String[] type = { "♠" , "♣" , "♥" , "♦"};
+	private String[] number={"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
 	
-	Card(){
-		for(int i=0;i<deck.length;i++){
-			deck[i] = i;
-			return deck[i];
+	Deck(){
+		int indexCards=0;
+		for(int i=0;i<type.length;i++){
+			for(int k=0;k<number.length;k++){
+				deck[indexCard] = new Card(type[i],number[k];
+				indexCard +=1;
+			}
 		}
 		
+	}
+	private Random r = new Random();
+	public void shuffle(){
+		Card temp;
+		for(int i = 0;i < 52;i++){
+			int a = r.nextInt(52);
+			temp = deck[i];
+			deck[i] = deck[a];
+			deck[a] = temp;
+		}
+		for(int i = 0;i<52;i++){
+			System.out.println(deck[i]);
+		}
+	}
+	public static void main(String[] args){
+		Deck deck1 = new Deck();
+		for(int i =0;i<52;i++){
+			System.out.println(deck1.Deck[i].getNum());
+		}
 	}
 }
