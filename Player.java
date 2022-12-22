8de6edd8019a687ce9d72847a57ca70970d3aede
Samet Tolga Esen <samettolga@gmail.com>
2022-12-23 00:55:36 +0300

    Player class changed and improved

--- Player.java
+++ Player.java
@@ -5,6 +5,15 @@ public class Player {
     public Player(String name) {
         PlayerName = name;
     }
+    //This set method sets the name of the player.
+     
+    public void setName(String name) {
+        this.PlayerName = name;
+    }
+    //This get method returns the String value of player name.
+    public String getName() {
+        return PlayerName;
+    }
 
 
 }
